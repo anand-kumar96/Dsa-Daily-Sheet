285f77546ccb7edb0654249705a8ddb276b3abac
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-03-07 02:30:25 +0530

    Update Set Matrix Zero.java

--- Day 01:Arrays Part-01/Set Matrix Zero.java
+++ Day 01:Arrays Part-01/Set Matrix Zero.java
@@ -15,7 +15,6 @@ class Solution {
        int row=matrix.length;
        int col=matrix[0].length;
         int arr[][]=new int[row][col];
-        System.out.println(row+" "+col);
        for(int i=0;i<row;i++){
            for(int j=0;j<col;j++){
                if(matrix[i][j]==0){
@@ -32,3 +31,61 @@ class Solution {
        }
     }
 }
+//method 02
+class Solution {
+    public void setZeroes(int[][] matrix) {
+       int row=matrix.length;
+       int col=matrix[0].length;
+        int R[]=new int[row];
+        int C[]=new int[col];
+       for(int i=0;i<row;i++){
+           for(int j=0;j<col;j++){
+               if(matrix[i][j]==0){
+                   R[i]=1;
+                   C[j]=1;
+               }
+           }
+       }
+         for(int i=0;i<row;i++){
+           for(int j=0;j<col;j++){
+               if(R[i]==1 || C[j]==1){
+                matrix[i][j]=0;
+               }
+           }
+       }
+    }
+}
+//for above both
+// Time Complexity: O(M*N), Traversing over the matrix two times.
+// Auxiliary Space: O(M + N), Taking two arrays one of size M and another of size N.
+
+// method 03
+class Solution {
+    public void setZeroes(int[][] matrix) {
+       int row=matrix.length;
+       int col=matrix[0].length;
+        boolean colVal=false;
+       for(int i=0;i<row;i++){
+           if(matrix[i][0]==0){
+                  colVal=true;
+              }
+           for(int j=1;j<col;j++){
+               if(matrix[i][j]==0){
+                   matrix[i][0]=matrix[0][j]=0;
+               }
+           }
+       }
+         for(int i=row-1;i>=0;i--){
+           for(int j=col-1;j>=1;j--){
+              if(matrix[i][0]==0 || matrix[0][j]==0) {
+                  matrix[i][j]=0;
+              }
+           }
+              if(colVal==true) {
+               matrix[i][0]=0;
+               }
+       }
+    }
+}
+// Time Complexity: O(M*N), Traversing over the matrix two times.
+// Auxiliary Space: O(1)
