67dc0d6d119f7ded836c817f381948882eec2f96
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-03-10 07:18:09 +0530

    Update Majority Element >N by 2 times.java

--- Day-03:Arrays Part-III/Majority Element >N by 2 times.java
+++ Day-03:Arrays Part-III/Majority Element >N by 2 times.java
@@ -1,4 +1,33 @@
 //https://leetcode.com/problems/majority-element/
+//Boyer-Moore Majority Voting Algorithm
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0;
+        int candidate=-1;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(count==0){
+                candidate=nums[i];
+                count=1;
+            }else if(candidate==nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+         count=0;
+        for(int i=0;i<n;i++){
+            if(candidate==nums[i]){
+                count++;
+            }
+        }
+        if(count>n/2){
+            return candidate;
+        }
+        return -1;
+    }
+}
+//method 01
 class Solution {
     public int majorityElement(int[] nums) {
         HashMap<Integer,Integer>hm=new HashMap<Integer,Integer>();
