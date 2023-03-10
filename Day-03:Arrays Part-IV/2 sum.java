d90102d7e4d48ffc42f41d1b279c7261a1342337
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-03-10 20:22:10 +0530

    Create 2 sum.java

---
+++ Day-03:Arrays Part-IV/2 sum.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/two-sum/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int ans[]=new int[2];
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(target-nums[i])){
+                ans[0]=hm.get(target-nums[i]);
+                ans[1]=i;
+                return ans;
+            }else{
+                hm.put(nums[i],i);
+            }
+        }
+        return ans;
+    }
+}
