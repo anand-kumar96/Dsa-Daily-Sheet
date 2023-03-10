d74faf747952ff7d382e62e3a2269556018f3f41
Anand kumar <106487247+Anand-kumar96@users.noreply.github.com>
2023-03-11 05:03:21 +0530

    Update Count Number Of SubArray withgiven Xor x.java

--- Day-04:Arrays Part-IV/Count Number Of SubArray withgiven Xor x.java
+++ Day-04:Arrays Part-IV/Count Number Of SubArray withgiven Xor x.java
@@ -17,3 +17,32 @@ public static int subarraysXor(ArrayList<Integer> arr, int x) {
 	   return ans;
 	}
 }
+// optimized
+//T:O(n) S:O(n)
+
+public class Solution {
+public static int subarraysXor(ArrayList<Integer> arr, int x) {
+	  int n=arr.size();
+	  int xor[]=new int[n];
+      xor[0]=arr.get(0);
+	  for(int i=1;i<arr.size();i++){
+		  xor[i]= xor[i-1]^arr.get(i);
+	  }
+	  HashMap<Integer,Integer>hm=new HashMap<>();
+	  long ans=0;
+	  for(int i=0;i<n;i++){
+		  int temp=x^xor[i];
+		  ans=ans+(hm.containsKey(temp)?(long)(hm.get(temp)):0);
+		  if(xor[i]==x){
+			  ans++;
+		  }
+		  if(hm.containsKey(xor[i])){
+			  hm.put(xor[i],hm.get(xor[i])+1);
+		  }else{
+			    hm.put(xor[i],1);
+		  }
+	  }
+	  return (int)ans;
+	  
+	}
+}
