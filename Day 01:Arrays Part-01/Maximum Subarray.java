f64986ae771439badb859a7a61953684f85505e5
Anand kumar <106487247+anand-kumar96@users.noreply.github.com>
2024-04-15 15:58:08 +0530

    Update Maximum Subarray.java

--- Day 01:Arrays Part-01/Maximum Subarray.java
+++ Day 01:Arrays Part-01/Maximum Subarray.java
@@ -22,3 +22,19 @@ class Solution {
         return max_so_far;
     }
 }
+
+/// or
+class Solution {
+    public int maxSubArray(int[] nums) {
+    int maxSum = Integer.MIN_VALUE;
+    int sum =0;
+        for(int i=0;i<nums.length;i++) {
+            sum += nums[i];
+            maxSum = Math.max(sum,maxSum);
+            if(sum<0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
