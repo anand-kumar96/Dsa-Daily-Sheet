1a6fed9ea407100e165e42da141a147bdd4616b2
Anand kumar <106487247+anand-kumar96@users.noreply.github.com>
2024-04-16 14:27:04 +0530

    Update Reverse Words in a StringII.java

--- Day 01:Arrays Part-01/Reverse Words in a StringII.java
+++ Day 01:Arrays Part-01/Reverse Words in a StringII.java
@@ -1,9 +1,5 @@
 //https://www.lintcode.com/problem/3648/
 public class Solution {
-    /**
-     * @param s: the char array
-     * @return: nothing
-     */
      public static void reverseList(List<Character>s,int low,int high){
      while(low<=high){
          char ch = s.get(low);
@@ -28,5 +24,31 @@ public class Solution {
           }
       }
       reverseList(s,low,high);
+    }
+}
+
+// or
+public class Solution {
+     public static void reverseList(List<Character>s,int low,int high){
+     while(low<=high){
+         char ch = s.get(low);
+         s.set(low, s.get(high));
+         s.set(high,ch);
+         low++;
+         high--;
+     }
+     }
+    public void reverseWords(List<Character> s) {
+      // reverse whole list
+      reverseList(s,0,s.size()-1);
+      // reverse each word
+      int low = 0;
+      for(int r=0;r<s.size();r++){
+          if(s.get(r)==' ') {
+           reverseList(s,low,r-1);
+           low = r+1;
+          }
+      }
+      reverseList(s,low,s.size()-1);
     }
 }
